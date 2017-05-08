454bfdf5cf460e0f94e846529b36ae86ddb933c5
Carlos Fernández <sirikon2@gmail.com>
2017-05-09 01:35:12 +0200

    Some changes

--- fuse.js
+++ fuse.js
@@ -2,25 +2,28 @@
 
 const { FuseBox, VuePlugin, SassPlugin, CSSPlugin, WebIndexPlugin, Sparky } = require('fuse-box')
 
-const fsbx = FuseBox.init({
+const fuse = FuseBox.init({
     homeDir: './src',
-    output: 'dist/app.js',
+    output: 'dist/$name.js',
     plugins: [
         VuePlugin(),
-        [SassPlugin({ importer: true }), CSSPlugin({ group: 'app.css', outFile: './dist/app.css' })],
+        [
+            SassPlugin({ importer: true }),
+            CSSPlugin({ group: 'app.css', outFile: './dist/app.css' })
+        ],
         WebIndexPlugin({ template: './src/index.html' })
     ]
 })
+
+fuse.dev()
+
+fuse.bundle('app.js')
+    .instructions('> index.ts')
+    .watch()
+
+fuse.run()
 
 Sparky.task('default', () => {
     return Sparky.watch('./assets', { base: './src' })
         .dest('./dist')
 })
-
-fsbx.dev()
-
-fsbx.bundle('app.js')
-    .instructions('>index.ts')
-    .watch()
-
-fsbx.run()
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 'use strict'
 
 import './style/main.scss'
-import * as Vue from 'vue/dist/vue.js'
+import * as Vue from 'vue/dist/vue.runtime.min.js'
 import * as App from './components/App.vue'
 
 new Vue({
